4d8624de076c05cb57db563b697390524eec1065
Davidsanch3z <130524021+Davidsanch3z@users.noreply.github.com>
2024-05-10 00:13:48 -0500

    Delete moda.js

--- moda.js
+++
@@ -1,7 +0,0 @@
-
-const typed = new Typed('.typed', {
-    strings: ['Ingenieria informatica...','Programación...', 'ciclismo...', 'rally...', 'estudiante...'],
-    typeSpeed:45,
-    backSpeed:60,
-    loop: true
-})
